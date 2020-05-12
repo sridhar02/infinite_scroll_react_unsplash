455dd50317eb471fc7080a8f8f0b28a3754b8284
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 00:06:33 +0530

    secrets masked

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = {
   APPLICATION_ID:
-    process.env.APPLICATION_ID || "nQ5zXBMVUV0bLM_Wi23kvQ0bpfuIRBdl0N3_kpZXSs0",
-  SECRET: process.env.SECRET || "TKd2OMCmK_qt3_kvY6U55iYpGM2z6VQuCMXB0VBQNhY",
+    process.env.APPLICATION_ID || "REPLACE",
+  SECRET: process.env.SECRET || "REPLACE",
   CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000",
 };
