e0a4ff91ad2811de6d4b2fdce6f42939ec97d77e
sridhar02 <kattasridhar02@gmail.com>
2020-05-12 23:21:05 +0530

    frontend proxy setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 function App() {
   return (
     <div id="root">
-      <div class="hero is-fullheight is-bold is-info">
-        <div class="hero-body">
-          <div class="container">
-            <div class="header content">
-              <h2 class="subtitle is-6">Code Challenge #16</h2>
-              <h1 class="title is-1">
+      <div className="hero is-fullheight is-bold is-info">
+        <div className="hero-body">
+          <div className="container">
+            <div className="header content">
+              <h2 className="subtitle is-6">Code Challenge #16</h2>
+              <h1 className="title is-1">
                 Infinite Scroll Unsplash Code Challenge
               </h1>
             </div>
-            <div class="images">
-              
+            <div className="images">
+
             </div>
           </div>
         </div>
