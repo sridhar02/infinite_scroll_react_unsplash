3cd4a30bb86b04d79c1c563d31aa9c98005d39e6
sridhar02 <kattasridhar02@gmail.com>
2020-05-12 23:14:05 +0530

    client side app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="root">
+      <div class="hero is-fullheight is-bold is-info">
+        <div class="hero-body">
+          <div class="container">
+            <div class="header content">
+              <h2 class="subtitle is-6">Code Challenge #16</h2>
+              <h1 class="title is-1">
+                Infinite Scroll Unsplash Code Challenge
+              </h1>
+            </div>
+            <div class="images">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
